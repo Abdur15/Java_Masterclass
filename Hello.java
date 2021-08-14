c5898d15d5d29d8527a3c75ed8c17475616feff7
Abdur Raquib N <54053501+Abdur15@users.noreply.github.com>
2021-08-14 10:48:08 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello Abdur");
+
+        int myFirstNumber = (5 + 10 + 3) + 2 * 10;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
+
+
+
+    }
+}
